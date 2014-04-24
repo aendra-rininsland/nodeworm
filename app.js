205f3444da54b5c7a2a5db9d6eeb2277f205e8e6
Ændrew Rininsland <aendrew@aendrew.com>
2014-04-24 03:36:52 +0100

    Almost there. Just need d3.js live output.

--- app.js
+++ app.js
@@ -1,26 +1,41 @@
-var app = require('express')()
+var express = require('express')
+  ,  app = express()
   , server = require('http').createServer(app)
-  , io = require('socket.io').listen(server);
+  , io = require('socket.io').listen(server)
+  , Tabletop = require('tabletop')
+  //, redis = require('redis-url').connect(process.env.REDISTOGO_URL)
+  ;
+
 
 Tabletop.init({ 
-  key: '__SPREADSHEET_KEY_HERE__',
-  callback: 'run_server',
-  simpleSheet: true 
+  key: '0Aqqh1cRUSxC-dHVFZi1LdWUzckcwd21TOUdFUXpaaGc',
+  simpleSheet: false,
+  callback: function (sheet, tabletop) {
+    server.listen(5544);
+
+    app.use('/client', express.static(__dirname + '/client'));
+
+    app.get('/', function (req, res) {
+      res.sendfile(__dirname + '/client/index.html');
+    });
+
+    // Poll every n seconds
+    setInterval(function(){
+      io.sockets.emit('dial_state_request');
+    },  process.env.NODEWORM_POLLING_DELAY || 5000);
+    
+
+    // Client connections
+    io.sockets.on('connection', function (client) {
+      client.on('dial_state_send', function (dial) {
+        //if (Date.now() > new Date(sheet.conf.elements[0].eventStart) && Date.now() < new Date(sheet.conf.elements[0].eventEnd)) {
+          //redis.set(client.id + '_' + Date.now(), dial.value);
+          console.dir([client.id,  Date.now(), dial.value]);
+        //}
+      });
+    });
+
+  }
 });
 
-function run_server (data, tabletop) {
-  server.listen(80);
-
-  app.get('/', function (req, res) {
-    res.sendfile(__dirname + '/index.html');
-  });
-
-  io.sockets.on('connection', function (socket) {
-    
-    socket.on('my other event', function (data) {
-      console.log(data);
-    });
-  });
-
-}
 
