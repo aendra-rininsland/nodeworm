de91785b72ffc86299f659bcaff8bd8314437657
Ændrew Rininsland <aendrew@aendrew.com>
2014-04-26 20:26:12 +0100

    First working version.

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var express = require('express')
-  ,  app = express()
+  , app = express()
   , server = require('http').createServer(app)
   , io = require('socket.io').listen(server)
-  , Tabletop = require('tabletop')
   , crypto = require('crypto')
+  , pg = require('pg')
+  , moment = require('moment')
   ;
 
 function generate_token (len) {
@@ -20,33 +21,53 @@ app.get('/', function (req, res) {
   res.sendfile(__dirname + '/client/index.html');
 });
 
-// Poll every n seconds
-setInterval(function(){
-  io.sockets.emit('dial_state_request');
-},  process.env.NODEWORM_POLLING_DELAY || 5000);
+app.get('/live', function (req, res) {
+  res.sendfile(__dirname + '/client/graph.html');
+});
 
+// Graph output connection
+var graph = io.of('/live').on('connection', function (socket) {
+  pg.connect(process.env.DATABASE_URL || "postgres://postgres:test123@localhost/postgres", function(err, pgClient) {
+    socket.on('livechart_begin_collection', function(clientInfo){
+      var ts = moment(clientInfo);
+      // Poll every n seconds
+      setInterval(function(){
+        // Get current timestamp.
+        pgClient.query({
+          name: 'nodeworm_livechart_polling',
+          text: "SELECT * FROM nodeworm WHERE ts > $1 AND type = 'pull'",
+          values: [ts.format("YYYY-MM-DD HH:mm:ss")]
+        }, function(err, res){
+          if (err) {
+            return console.error('Error running query', err);
+          }
+          var avg = 0;
+          res.rows.forEach(function(v){
+            avg += v.value;
+          });
+          socket.emit('send_livechart_data', {worm: avg / res.rows.length});
+          ts = moment();
+        });
+      },  2000);
+    });
+  });
+}); //end /live
 
 // Client connections
-io.sockets.on('connection', function (client) {
-  client.on('livechart_begin_collection', function(client){
-    var polls = [];
-
-    // Poll every n seconds
-    setInterval(function(){
-      var token = generate_token(10);
-      polls.push({id: token, responses: []});
-      io.sockets.emit('livechart_polling', {token: token});
-      client.on('livechart_polling_response', function(res){
-
+var client = io.of('/client');
+client.on('connection', function (socket) {
+  pg.connect(process.env.DATABASE_URL || "postgres://postgres:test123@localhost/postgres", function(err, pgClient) {
+    socket.on('dial_state_send', function (dial) {
+      pgClient.query({
+        name: 'nodeworm_client_submission',
+        text: 'INSERT INTO nodeworm("clientid", "ts", "value", "type") VALUES ($1, $2, $3, $4)',
+        values: [socket.id, moment().format("YYYY-MM-DD HH:mm:ss"), dial.value, dial.type]
       });
-    },  process.env.NODEWORM_POLLING_DELAY || 1000);
+    });
   });
+}); //end /client
 
-  client.on('dial_state_send', function (dial) {
-    //if (Date.now() > new Date(sheet.conf.elements[0].eventStart) && Date.now() < new Date(sheet.conf.elements[0].eventEnd)) {
-      //redis.set(client.id + '_' + Date.now(), dial.value);
-      console.dir([client.id,  Date.now(), dial.value]);
-    //}
-  });
-
-}); //end on('connection')
+// Poll every n seconds
+setInterval(function(){
+  client.emit('dial_state_request');
+}, 1000);
